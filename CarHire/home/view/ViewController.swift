4cd8159aaadca439f7059a3def13a96889d2bb61
Andrew Ananda <andrewtiema@gmail.com>
2021-06-12 18:47:20 +0300

    fixes

--- CarHire/home/view/ViewController.swift
+++ CarHire/home/view/ViewController.swift
@@ -11,13 +11,14 @@ import RxRelay
 
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
 
-    @IBOutlet weak var collectionView: UICollectionViewCell!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var topDealsCollectionView: UICollectionView!
     private let viewModel = HomeViewModel()
     private let bag = DisposeBag()
     
     private var featuredCar = BehaviorRelay<FeaturedCar?>(value: nil)
     private var topDealData = BehaviorRelay<FeaturedCar?>(value: nil)
+    private var data = [FeaturedCar]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,20 +57,39 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
 extension ViewController {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        if collectionView == self.collectionView {
+            return 1
+        }
+        else {
+            topDealData.subscribe(onNext: {[weak self] response in
+                if let data = response{
+                    self?.data.append(data)
+                }
+            }).disposed(by: bag)
+            return 10
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.viewConstants.reusableFeaturedCarCell, for: indexPath) as! FeaturedCarViewCell
-        featuredCar.subscribe(onNext: {response in
-            cell.carName.text = response?.model?.title
-            cell.plateNumber.text = response?.number_plate
-            guard let imageUrl = response?.photo else {
-                return
-            }
-            cell.loadImage(url: imageUrl)
-        }).disposed(by: bag)
+        if collectionView == self.collectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.viewConstants.reusableFeaturedCarCell, for: indexPath) as! FeaturedCarViewCell
+            featuredCar.subscribe(onNext: {response in
+                cell.carName.text = response?.model?.title
+                cell.plateNumber.text = response?.number_plate
+                guard let imageUrl = response?.photo else {
+                    return
+                }
+                cell.loadImage(url: imageUrl)
+            }).disposed(by: bag)
 
-        return cell
+            return cell
+        }
+        else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.viewConstants.topDealsCell, for: indexPath) as! TopViewCell
+            topDealData.subscribe(onNext: {response in
+
+            }).disposed(by: bag)
+            return cell
+        }
     }
 }
