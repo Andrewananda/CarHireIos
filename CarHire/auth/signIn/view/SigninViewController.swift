7f85721fa037a826e8be3d3420e944050d67b1e7
andrewtiema@gmail.com <andrewtiema@gmail.com>
2021-10-07 12:18:59 +0300

    login Screen

--- CarHire/auth/signIn/view/SigninViewController.swift
+++ CarHire/auth/signIn/view/SigninViewController.swift
@@ -7,23 +7,68 @@
 
 import UIKit
 
-class SigninViewController: UIViewController {
+class SigninViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var shouldNotChangeUserName: Bool = false
+    var shouldNotChangePassword: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        txtPassword.delegate = self
+        txtUserName.delegate = self
         self.navigationController?.isNavigationBarHidden = false
+        
+        txtPassword.textColor = .gray
+        txtUserName.textColor = .gray
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        switch textField {
+        case txtUserName:
+            if !shouldNotChangeUserName {
+                txtUserName.text = ""
+                txtUserName.textColor = .black
+            }
+        case txtPassword:
+            if !shouldNotChangePassword {
+                txtPassword.text = ""
+                txtPassword.isSecureTextEntry = true
+                txtPassword.textColor = .black
+            }
+        default:
+            txtPassword.text = ""
+            txtPassword.textColor = .black
+        }
     }
-    */
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        switch textField {
+        case txtUserName:
+            if txtUserName.text == "" {
+                txtUserName.text = "Enter Email / Phone"
+                txtUserName.textColor = .gray
+                shouldNotChangeUserName = false
+            }else {
+                shouldNotChangeUserName = true
+            }
+        case txtPassword:
+            if txtPassword.text == "" {
+                txtPassword.isSecureTextEntry =  false
+                txtPassword.text = "Enter Password"
+                txtPassword.textColor = .gray
+                shouldNotChangePassword = false
+            }else {
+                shouldNotChangePassword = true
+            }
+        default:
+            if txtPassword.text == "" {
+                txtPassword.text = "Enter Password"
+                txtPassword.textColor = .gray
+            }
+        }
+    }
 
 }
